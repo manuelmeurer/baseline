60bbb3b52784fa9fe522235e9e54fb5cd6f272cd
Manuel Meurer <manuel@meurer.io>
2023-12-10 14:14:56 +0100

    add deep_fetch

---
+++ lib/baseline/deep_fetch.rb
@@ -0,0 +1,17 @@
+module DeepFetch
+  def deep_fetch(*keys, &block)
+    keys.inject(self) {
+      _1.fetch(_2)
+    }
+  rescue KeyError => error
+    if block_given?
+      block.call
+    else
+      raise error
+    end
+  end
+end
+
+class Hash
+  include DeepFetch
+end
--- lib/baseline/railtie.rb
+++ lib/baseline/railtie.rb
@@ -5,6 +5,7 @@ module Baseline
       require "baseline/external_service"
       require "baseline/report_error"
       require "baseline/has_timestamps"
+      require "baseline/deep_fetch"
 
       Rails.application.reloader.to_prepare do
         Current.missing_value = "_missing_value_"
