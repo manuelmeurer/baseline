557e10e59f32c7e17212c73db94e72a683d0dec7
Manuel Meurer <manuel@meurer.io>
2024-06-17 13:57:07 +0200

    allow calling `if` with procs generated from symbols

--- lib/baseline/if.rb
+++ lib/baseline/if.rb
@@ -1,16 +1,23 @@
 # frozen_string_literal: true
 
 class Object
-  def if(condition, action = nil)
-    result = condition.is_a?(Proc) ? condition.call(self) : condition
+  def if(condition, action = nil, &block)
+    result = condition.is_a?(Proc) ?
+      condition.call(self) :
+      condition
+
     return self unless result
+
     if action
-      if block_given?
+      if block
         raise "You can't pass both a block and an action."
       end
       action
     else
-      yield self, result
+      args = block.arity < 0 ?
+        [self] :
+        [self, result].take(block.arity)
+      block.call *args
     end
   end
 end
