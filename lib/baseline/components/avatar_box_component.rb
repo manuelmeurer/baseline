5e507c9625351c349cd667a8f2d9c8dd2a6e4987
Manuel Meurer <manuel@meurer.io>
2025-06-25 23:02:41 +0200

    bit more margin

--- lib/baseline/components/avatar_box_component.rb
+++ lib/baseline/components/avatar_box_component.rb
@@ -4,6 +4,11 @@ class AvatarBoxComponent < ApplicationComponent
   def initialize(imageable, size:, vertical: false, rounded_corners: true, image_wrapper: nil)
     @imageable, @size, @vertical, @rounded_corners, @image_wrapper =
       imageable, size, vertical, rounded_corners, image_wrapper
-    @margin = size.in?(%i[xs sm]) ? 2 : 3
+    @margin = {
+      xs: 2,
+      sm: 2,
+      md: 3,
+      lg: 3
+    }.fetch(size, 4)
   end
 end
