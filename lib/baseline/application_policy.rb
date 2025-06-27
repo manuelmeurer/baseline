19290c083250a78dd8e56bfc866c4a87d4832125
Manuel Meurer <manuel@meurer.io>
2025-06-27 12:02:38 +0200

    add avo association policy handling

--- lib/baseline/application_policy.rb
+++ lib/baseline/application_policy.rb
@@ -2,6 +2,35 @@
 
 module Baseline
   class ApplicationPolicy
+    include ::Avo::Pro::Concerns::PolicyHelpers
+
+    # When a policy inherits from this class, check all associations of the policy class
+    # and call Avo`s `inherit_association_from_policy` for those that have a corresponding policy class.
+    # https://docs.avohq.io/3.0/authorization.html#removing-duplication
+    def self.inherited(subclass)
+      klass = subclass
+        .to_s
+        .delete_suffix("Policy")
+        .constantize
+
+      klass
+        .reflections
+        .select { _2.collection? }
+        .keys
+        .without(%w[versions]) # Papertrail versions
+        .each do |association|
+          policy_klass = klass
+            .reflect_on_association(association)
+            .class_name
+            .then { "#{_1}Policy" }
+            .safe_constantize
+          next unless policy_klass
+          inherit_association_from_policy \
+            association,
+            policy_klass
+        end
+    end
+
     attr_reader :user, :record
 
     def initialize(user, record)
