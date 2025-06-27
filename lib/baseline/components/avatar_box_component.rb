70705c8c53ddb54f8c3b24b85418422d45d440e7
Manuel Meurer <manuel@meurer.io>
2025-06-27 12:02:58 +0200

    whitespace

--- lib/baseline/components/avatar_box_component.rb
+++ lib/baseline/components/avatar_box_component.rb
@@ -4,6 +4,7 @@ class AvatarBoxComponent < ApplicationComponent
   def initialize(imageable, size:, vertical: false, rounded_corners: true, image_wrapper: nil)
     @imageable, @size, @vertical, @rounded_corners, @image_wrapper =
       imageable, size, vertical, rounded_corners, image_wrapper
+
     @margin = {
       xs: 2,
       sm: 2,
