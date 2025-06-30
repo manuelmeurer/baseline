e03847f9a29648d05e764158527da0871b121f04
Manuel Meurer <manuel@meurer.io>
2025-06-30 11:34:13 +0200

    add app name when posting notification to slack

--- lib/baseline/services/notifications/create.rb
+++ lib/baseline/services/notifications/create.rb
@@ -15,44 +15,34 @@ module Baseline
 
         notification = Notification.create!(args)
         notifiable   = notification.notifiable
-        message = [
-          "*#{notification.title}*",
-          notification.details,
-          if notifiable
-            admin_url =
-              defined?(::Avo) ?
-                ::Avo::Engine
-                  .routes
-                  .url_helpers
-                  .url_for([
-                    :resources,
-                    notifiable
-                  ]) :
-                url_for([:admin, notifiable])
-            [
-              %(#{notifiable.model_name.human} "#{notifiable}"),
-              admin_url
-            ]
-          end
-        ].flatten
-          .compact
-          .join("\n")
+        message_parts = [
+          "*#{notification.title} [#{Rails.application.class.module_parent_name.titleize}]*",
+          notification.details
+        ]
+        if notifiable
+          admin_url =
+            defined?(::Avo) ?
+              ::Avo::Engine
+                .routes
+                .url_helpers
+                .url_for([
+                  :resources,
+                  notifiable
+                ]) :
+              url_for([:admin, notifiable])
+          message_parts.concat [
+            %(#{notifiable.model_name.human} "#{notifiable}"),
+            admin_url
+          ]
+        end
 
-        post_to_slack \
-          notification,
-          message
+        Baseline::External::SlackSimple.call_async \
+          :post_message,
+          SLACK_CHANNEL_ID,
+          message_parts.join("\n")
 
         notification
       end
-
-      private
-
-        def post_to_slack(notification, message)
-          Baseline::External::SlackSimple.call_async \
-            :post_message,
-            SLACK_CHANNEL_ID,
-            message
-        end
     end
   end
 end
