79965eedb20334da3e72d64a9af675a24fed4a81
Manuel Meurer <manuel@meurer.io>
2025-07-07 13:20:26 +0200

    refactor contact request form component

--- lib/baseline/components/contact_request_form_component.rb
+++ lib/baseline/components/contact_request_form_component.rb
@@ -22,5 +22,29 @@ module Baseline
           t(field, scope: %i[web contact_request_form])
       end
     end
+
+    def render_field(form, field)
+      render_field_tag = ->(_form) do
+        field_type =
+          field.in?(%i[message links]) ?
+            :text_area :
+            (field == :email ? :email : :text)
+
+        options = {
+          required: field.in?(%i[name email locations employees message links referral]),
+          data:     (helpers.stimco(:autosize) if field_type == :text_area)
+        }.compact
+
+        form_field _form, field_type, field, label_style: :floating, label: @texts.fetch(field), **options
+      end
+
+      if ContactRequest.column_names.include?(field.to_s)
+        render_field_tag.call(form)
+      else
+        form.fields_for :details do |details_form|
+          render_field_tag.call(details_form)
+        end
+      end
+    end
   end
 end
