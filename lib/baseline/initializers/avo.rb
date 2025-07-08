9487ca25276450afbf5ad6d53cbee3cceca8c1d0
Manuel Meurer <manuel@meurer.io>
2025-07-08 23:43:24 +0200

    add avo initializer

---
+++ lib/baseline/initializers/avo.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require "avo"
+
+Avo.configure do |config|
+  config.app_name                      = "Admin Dashboard"
+  config.authorization_client          = :pundit
+  config.click_row_to_view_record      = true
+  config.currency                      = "EUR"
+  config.explicit_authorization        = true
+  config.license_key                   = Rails.application.env_credentials.avo.license_key!
+  config.raise_error_on_missing_policy = true
+  config.root_path                     = "cms"
+
+  config.current_user_method do
+    Current.admin_user
+  end
+end
