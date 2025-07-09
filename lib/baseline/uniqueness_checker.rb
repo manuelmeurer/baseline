72f0e38198d562dc26185e4270be31b2eb794a56
Manuel Meurer <manuel@meurer.io>
2025-07-09 22:48:29 +0200

    formatting

--- lib/baseline/uniqueness_checker.rb
+++ lib/baseline/uniqueness_checker.rb
@@ -45,12 +45,11 @@ module Baseline
       end
 
       if @_similar_service_id = Rails.cache.read(new_uniqueness_key)
-        if on_error.to_sym == :ignore
-          return false
-        else
-          @_retries_exhausted = on_error.to_sym == :reschedule && error_count >= MAX_RETRIES
-          raise_not_unique_error
-        end
+        return false if on_error.to_sym == :ignore
+        @_retries_exhausted =
+          on_error.to_sym == :reschedule &&
+          error_count >= MAX_RETRIES
+        raise_not_unique_error
       else
         @_uniqueness_keys ||= []
         @_uniqueness_keys << new_uniqueness_key
@@ -88,72 +87,72 @@ module Baseline
 
     private
 
-    def raise_not_unique_error
-      message = [
-        "Service #{self.class} #{@id} with uniqueness args #{@_uniqueness_args} is not unique, a similar service is already running: #{@_similar_service_id}.",
-        ("The service has been retried #{MAX_RETRIES} times." if @_retries_exhausted)
-      ].compact
-        .join(" ")
+      def raise_not_unique_error
+        message = [
+          "Service #{self.class} #{@id} with uniqueness args #{@_uniqueness_args} is not unique, a similar service is already running: #{@_similar_service_id}.",
+          ("The service has been retried #{MAX_RETRIES} times." if @_retries_exhausted)
+        ].compact
+          .join(" ")
 
-      raise self.class::NotUniqueError.new(message)
-    end
+        raise self.class::NotUniqueError.new(message)
+      end
 
-    def convert_for_rescheduling(arg)
-      case arg
-      when Array
-        arg.map do |array_arg|
-          convert_for_rescheduling array_arg
+      def convert_for_rescheduling(arg)
+        case arg
+        when Array
+          arg.map do |array_arg|
+            convert_for_rescheduling array_arg
+          end
+        when Integer, String, TrueClass, FalseClass, NilClass
+          arg
+        when object_class
+          arg.id
+        else
+          raise "Don't know how to convert arg #{arg.inspect} for rescheduling."
         end
-      when Integer, String, TrueClass, FalseClass, NilClass
-        arg
-      when object_class
-        arg.id
-      else
-        raise "Don't know how to convert arg #{arg.inspect} for rescheduling."
       end
-    end
 
-    def reschedule
-      # Convert service args for rescheduling first
-      reschedule_args = @_service_args.map do |arg|
-        convert_for_rescheduling arg
+      def reschedule
+        # Convert service args for rescheduling first
+        reschedule_args = @_service_args.map do |arg|
+          convert_for_rescheduling arg
+        end
+        log :info, "Rescheduling", seconds: retry_delay
+        self.class.call_in retry_delay, *reschedule_args
       end
-      log :info, "Rescheduling", seconds: retry_delay
-      self.class.call_in retry_delay, *reschedule_args
-    end
 
-    def error_count
-      (Rails.cache.read(error_count_key) || 0).to_i
-    end
+      def error_count
+        (Rails.cache.read(error_count_key) || 0).to_i
+      end
 
-    def increase_error_count
-      Rails.cache.write \
-        error_count_key,
-        error_count + 1,
-        expires_in: retry_delay + THIRTY_DAYS
-    end
+      def increase_error_count
+        Rails.cache.write \
+          error_count_key,
+          error_count + 1,
+          expires_in: retry_delay + THIRTY_DAYS
+      end
 
-    def uniqueness_key(args)
-      [
-        KEY_PREFIX,
-        self.class.to_s.gsub(":", "_"),
-        (Digest::MD5.hexdigest(args.to_s) unless args.empty?)
-      ].compact
-        .join(":")
-    end
+      def uniqueness_key(args)
+        [
+          KEY_PREFIX,
+          self.class.to_s.gsub(":", "_"),
+          (Digest::MD5.hexdigest(args.to_s) unless args.empty?)
+        ].compact
+          .join(":")
+      end
 
-    def error_count_key
-      [
-        KEY_PREFIX,
-        "errors",
-        self.class.to_s.gsub(":", "_")
-      ].tap do |key|
-        key << Digest::MD5.hexdigest(@_service_args.to_s) unless @_service_args.empty?
-      end.join(":")
-    end
+      def error_count_key
+        [
+          KEY_PREFIX,
+          "errors",
+          self.class.to_s.gsub(":", "_")
+        ].tap do |key|
+          key << Digest::MD5.hexdigest(@_service_args.to_s) unless @_service_args.empty?
+        end.join(":")
+      end
 
-    def retry_delay
-      error_count ** 3 + 5
-    end
+      def retry_delay
+        error_count ** 3 + 5
+      end
   end
 end
