378e0476a6bfac5bc4792a8b3e33f97e1fd38c54
Manuel Meurer <manuel@meurer.io>
2025-07-16 08:15:22 +0200

    rename var

--- lib/baseline/routes.rb
+++ lib/baseline/routes.rb
@@ -2,31 +2,31 @@
 
 module Baseline
   module Routes
-    def self.extended(mod)
-      mod.concern :errors do
+    def self.extended(routes)
+      routes.concern :errors do
         match ":id",
           to:  "errors#show",
           via: :all,
           id:  /\d{3}/
       end
 
-      mod.concern :health do
+      routes.concern :health do
         get "up" => "/rails/health#show", as: :rails_health_check
       end
 
-      mod.concern :sitemap do
+      routes.concern :sitemap do
         get :sitemap,
           controller: "base",
           defaults:   { format: "xml" },
           format:     true
       end
 
-      mod.concern :pwa do
+      routes.concern :pwa do
         get :manifest,
           controller: "base"
       end
 
-      mod.concern :auth do
+      routes.concern :auth do
         scope controller: :sessions do
           get    :login,  action: "new"
           post   :login,  action: "create"
@@ -35,7 +35,7 @@ module Baseline
       end
 
       %i[allow disallow].each do |action|
-        mod.concern :"#{action}_robots" do
+        routes.concern :"#{action}_robots" do
           get :robots,
             controller: "base",
             id:         action,
