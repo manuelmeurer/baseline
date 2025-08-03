1a22851a494d1dd457ca54f01ea50fe22cd3783d
Manuel Meurer <manuel@meurer.io>
2025-08-03 11:19:58 +0200

    fix adding flash query param

--- lib/baseline/controller_concerns/web_base_controllable.rb
+++ lib/baseline/controller_concerns/web_base_controllable.rb
@@ -10,19 +10,26 @@ module Baseline
       skip_forgery_protection
 
       after_action do
-        if flash.keys.any? && response.redirect? && web_url?(response.location)
-          Addressable::URI
-            .parse(response.location)
-            .tap { _1.query_values = Hash(_1.query_values).merge(flash: nil) }
-            .then { response.location = _1.to_s }
-        end
-
         if flash.keys.any? || params.key?(:flash)
           expires_now
         else
           request.session_options[:skip] = true
         end
       end
+    end
+
+    def html_redirect_to(options = {}, response_options = {})
+      if flash.keys.any?
+        url = url_for(options)
+        if web_url?(url)
+          url = Addressable::URI
+            .parse(url)
+            .tap { _1.query_values = Hash(_1.query_values).merge(flash: nil) }
+            .to_s
+        end
+      end
+
+      super url, response_options
     end
 
     private def web_url?(url)
