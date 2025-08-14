87f19693286bc17d5824522e92d1fb9bff5b809d
Manuel Meurer <manuel@meurer.io>
2025-08-14 15:52:37 +0200

    add form actions component

--- lib/baseline/components/form_actions_component.rb
+++ lib/baseline/components/form_actions_component.rb
@@ -2,13 +2,31 @@
 
 module Baseline
   class FormActionsComponent < ApplicationComponent
-    def initialize(form, horizontal: false, submit_label: nil, submit_data: {}, button_color: Current.default_button_color)
-      @form, @horizontal, @submit_label, @submit_data, @button_color =
-        form, horizontal, submit_label, submit_data, button_color
+    def initialize(
+      form,
+      style =       :save,
+      horizontal:   false,
+      submit_label: nil,
+      submit_icon:  nil,
+      submit_data:  {},
+      button_color: Current.default_button_color,
+      button_size:  nil)
+
+      @form, @style, @horizontal, @submit_data, @submit_label, @submit_icon, @button_color, @button_size =
+        form, style, horizontal, submit_data, submit_label, submit_icon, button_color, button_size
     end
 
     def before_render
-      @submit_label ||= t(:save)
+      case @style
+      when :save
+        @submit_label ||= t(:save).capitalize
+        @submit_icon  ||= "circle-check"
+      when :submit
+        @submit_label ||= t(:submit).capitalize
+        @submit_icon  ||= "paper-plane"
+      else
+        raise "Unexpected style: #{style}"
+      end
     end
   end
 end
