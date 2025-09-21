3ca8ee8012c12216498cf8eb940e5761f90dc22a
Manuel Meurer <manuel@meurer.io>
2025-09-21 11:42:47 +0200

    load locale from current user

--- lib/baseline/controller_concerns/set_locale.rb
+++ lib/baseline/controller_concerns/set_locale.rb
@@ -2,43 +2,33 @@
 
 module Baseline
   module SetLocale
-    class << self
-      def [](locale_proc = nil)
-        Module.new do
-          extend ActiveSupport::Concern
+    extend ActiveSupport::Concern
 
-          included do
-            around_action do |_, block|
-              locale =
-                params[:locale] ||
-                cookies[:locale] ||
-                locale_proc&.call ||
-                locale_from_accept_language_header
+    included do
+      around_action do |_, block|
+        locale =
+          params[:locale] ||
+          cookies[:locale] ||
+          ::Current.user&.locale ||
+          locale_from_accept_language_header
 
-              locale = locale&.to_sym
+        locale = locale&.to_sym
 
-              unless locale&.in?(I18n.available_locales)
-                locale = I18n.default_locale
-              end
-
-              I18n.with_locale locale, &block
-            end
-
-            private
-
-              def locale_from_accept_language_header
-                request.headers["Accept-Language"]
-                  &.split(",")
-                  &.map { _1.split(";").first }
-                  &.detect { I18n.available_locales.include?(_1.to_sym) }
-              end
-          end
+        unless locale&.in?(I18n.available_locales)
+          locale = I18n.default_locale
         end
+
+        I18n.with_locale locale, &block
       end
 
-      def included(base)
-        base.include self[]
-      end
+      private
+
+        def locale_from_accept_language_header
+          request.headers["Accept-Language"]
+            &.split(",")
+            &.map { _1.split(";").first }
+            &.detect { I18n.available_locales.include?(_1.to_sym) }
+        end
     end
   end
 end
