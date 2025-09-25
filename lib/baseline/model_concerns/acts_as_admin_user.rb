53fd78c23dccda4b54281f32b5f938b82fe20eb0
Manuel Meurer <manuel@meurer.io>
2025-09-25 08:40:06 +0200

    generate email signature for new admin as well

--- lib/baseline/model_concerns/acts_as_admin_user.rb
+++ lib/baseline/model_concerns/acts_as_admin_user.rb
@@ -2,6 +2,12 @@
 
 module Baseline
   module ActsAsAdminUser
-    def email_signature = AdminUsers::GenerateEmailSignature.call(self)
+    extend ActiveSupport::Concern
+
+    class_methods do
+      delegate :email_signature, to: :new
+    end
+
+    def email_signature = _do_generate_email_signature
   end
 end
