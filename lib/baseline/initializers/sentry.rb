4685e8fa6f0c57ddaaa95738a1111616b8478dbe
Manuel Meurer <manuel@meurer.io>
2025-10-02 12:04:46 +0200

    use string

--- lib/baseline/initializers/sentry.rb
+++ lib/baseline/initializers/sentry.rb
@@ -5,7 +5,7 @@ require "sentry-rails"
 Sentry.init do |config|
   config.breadcrumbs_logger      = %i[active_support_logger http_logger]
   config.dsn                     = Rails.application.env_credentials.sentry&.dsn || "dummy"
-  config.excluded_exceptions    -= [ActiveRecord::RecordNotFound.to_s]
+  config.excluded_exceptions    -= ["ActiveRecord::RecordNotFound"]
   config.include_local_variables = true
   config.release                 = Rails.configuration.revision
   config.send_default_pii        = true
