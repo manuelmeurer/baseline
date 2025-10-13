07be6f17c024baba1853d855830b3bcdef1a310b
Manuel Meurer <manuel@meurer.io>
2025-10-13 13:42:19 +0200

    only in development

--- lib/baseline/environments/development.rb
+++ lib/baseline/environments/development.rb
@@ -3,6 +3,8 @@
 Warning[:deprecated] = true
 
 Rails.application.configure do
+  config.hotwire.spark.html_extensions += %w[haml]
+
   config.hosts.push(
     *Rails.application.env_credentials.host!.then { [_1, ".#{_1}"] }
   )
