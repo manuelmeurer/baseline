b51cfe2be703ea901cde1d7b5d45d81fb2b827cd
Manuel Meurer <manuel@meurer.io>
2025-10-14 09:58:20 +0200

    allow setting header css class and header data for card component

--- lib/baseline/components/card_component.rb
+++ lib/baseline/components/card_component.rb
@@ -2,9 +2,9 @@
 
 module Baseline
   class CardComponent < ApplicationComponent
-    def initialize(title: nil, image: nil, icon: nil, icon_position: :center, header: nil, css_class: nil, body_css_class: nil, icon_css_class: nil, equal_height: false, url: nil, footer: nil, footer_css_class: nil, title_tag: :h5, above_title: nil, after_body: nil, image_cover: false, data: {}, style: nil)
-      @title, @image, @icon, @icon_position, @header, @css_class, @body_css_class, @icon_css_class, @equal_height, @url, @footer, @footer_css_class, @title_tag, @above_title, @after_body, @image_cover, @data, @style =
-        title, image, icon, icon_position, header, css_class, body_css_class, icon_css_class, equal_height, url, footer, footer_css_class, title_tag, above_title, after_body, image_cover, data, style
+    def initialize(title: nil, image: nil, icon: nil, icon_position: :center, header: nil, header_css_class: nil, header_data: {}, css_class: nil, body_css_class: nil, icon_css_class: nil, equal_height: false, url: nil, footer: nil, footer_css_class: nil, title_tag: :h5, above_title: nil, after_body: nil, image_cover: false, data: {}, style: nil)
+      @title, @image, @icon, @icon_position, @header, @header_css_class, @header_data, @css_class, @body_css_class, @icon_css_class, @equal_height, @url, @footer, @footer_css_class, @title_tag, @above_title, @after_body, @image_cover, @data, @style =
+        title, image, icon, icon_position, header, header_css_class, header_data, css_class, body_css_class, icon_css_class, equal_height, url, footer, footer_css_class, title_tag, above_title, after_body, image_cover, data, style
 
       unless @icon_position.in?(%i[center left])
         raise ArgumentError, "icon_position must be :center or :left"
