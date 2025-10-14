891e95bb8f20ed6df60bd41e6c04275586a618c8
Manuel Meurer <manuel@meurer.io>
2025-10-14 09:59:15 +0200

    configure hotwire spark to watch components for html changes

--- lib/baseline/environments/development.rb
+++ lib/baseline/environments/development.rb
@@ -4,6 +4,7 @@ Warning[:deprecated] = true
 
 Rails.application.configure do
   config.hotwire.spark.html_extensions += %w[haml]
+  config.hotwire.spark.html_paths      += %w[app/components]
 
   config.hosts.push(
     *Rails.application.env_credentials.host!.then { [_1, ".#{_1}"] }
