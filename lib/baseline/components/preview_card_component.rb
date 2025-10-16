ff8bff8f4982351a262f7d3f0451ed32fa64bcdd
Manuel Meurer <manuel@meurer.io>
2025-10-16 22:32:47 +0200

    render youtube videos in preview cards

--- lib/baseline/components/preview_card_component.rb
+++ lib/baseline/components/preview_card_component.rb
@@ -8,14 +8,35 @@ module Baseline
     end
 
     def before_render
-      require "baseline/services/external/link_metadata"
+      @type =
+        case @url
+        when /youtube\.com/ then :youtube
+        else :card
+        end
 
-      @url         = helpers.normalize_url(@url)
-      metadata     = External::LinkMetadata.get_metadata(@url, cache: 1.year)
-      @title       = CGI.unescapeHTML(CGI.unescapeHTML(metadata.fetch(:title)))
-      @description = CGI.unescapeHTML(CGI.unescapeHTML(metadata.fetch(:description)))
-      @image_url   = CGI.unescapeHTML(metadata.deep_fetch(:image, :url))
-      @website     = IdentifyURL.call(@url).name
+      suppress NoMethodError do
+        send "before_render_#{@type}"
+      end
     end
+
+    private
+
+      def before_render_card
+        require "baseline/services/external/link_metadata"
+
+        @url         = helpers.normalize_url(@url)
+        metadata     = External::LinkMetadata.get_metadata(@url, cache: 1.year)
+        @title       = metadata.fetch(:title)&.then { CGI.unescapeHTML(CGI.unescapeHTML(_1)) }
+        @description = metadata.fetch(:description)&.then { CGI.unescapeHTML(CGI.unescapeHTML(_1)) }
+        @image_url   = metadata.dig(:image, :url)&.then { CGI.unescapeHTML(_1) }
+        @website     = IdentifyURL.call(@url).name
+      end
+
+      def before_render_youtube
+        unless youtube_id = @url[%r{/watch\?v=(.+)}, 1]
+          raise "Could not determine YouTube ID from URL: #{@url}"
+        end
+        @url = "https://www.youtube.com/embed/#{youtube_id}"
+      end
   end
 end
