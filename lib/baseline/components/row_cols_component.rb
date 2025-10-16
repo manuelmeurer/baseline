4eb116d37c9b2cb76dbdc94bb3fe7cb0135a5eb7
Manuel Meurer <manuel@meurer.io>
2025-10-16 11:23:53 +0200

    convert row_cols partial to component

---
+++ lib/baseline/components/row_cols_component.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module Baseline
+  class RowColsComponent < ApplicationComponent
+    renders_many :cols, "ColComponent"
+
+    def initialize(cols: { sm: 2, lg: 3, xl: 4 }, gutter: { nil => 3, md: 4 }, css_class: nil, id: nil, data: nil)
+      if css_class&.if(Array) { _1.join(" ") }&.match?(/\bm[ty]?-/)
+        ReportError.call "Don't set a top margin on row cols, it messes up the negative top margin that is added by Bootstrap to offset the gutter."
+      end
+
+      @cols, @gutter, @css_class, @id, @data =
+        cols, gutter, css_class, id, data
+
+      @css_class = gutter
+        .if(Integer, { nil => gutter })
+        .map { ["g", _1, _2].compact.join("-") }
+        .concat(@cols.map { ["row-cols", _1, _2].join("-") })
+        .push("row", "row-cols-1", *@css_class)
+    end
+
+    def call
+      tag.div class: @css_class, id: @id, data: @data do
+        content
+      end
+    end
+
+    class ColComponent < ApplicationComponent
+      def call
+        tag.div class: "col" do
+          content
+        end
+      end
+    end
+  end
+end
