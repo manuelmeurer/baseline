6db0fd431fe4b393c96bf20551d00ba1cf2ba34f
Manuel Meurer <manuel@meurer.io>
2025-10-22 22:10:43 +0200

    work on admin user stuff

--- lib/baseline/model_concerns/acts_as_admin_user.rb
+++ lib/baseline/model_concerns/acts_as_admin_user.rb
@@ -4,6 +4,23 @@ module Baseline
   module ActsAsAdminUser
     extend ActiveSupport::Concern
 
+    included do
+      store_accessor :tokens,
+        :todoist_access_token,
+        :google_access_token,
+        :google_refresh_token
+
+      validate do
+        invalid_alternate_emails = alternate_emails.select { EmailValidator.invalid? _1 }
+        case
+        when invalid_alternate_emails.any?
+          errors.add :alternate_emails, message: "contain invalid elements: #{invalid_alternate_emails.join(", ")}"
+        when email.present? && alternate_emails.include?(email)
+          errors.add :alternate_emails, message: "must not contain email #{email}"
+        end
+      end
+    end
+
     class_methods do
       delegate :email_signature, to: :new
     end
---
+++ lib/baseline/model_concerns/user_proxy.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Baseline
+  module UserProxy
+    extend ActiveSupport::Concern
+
+    FIELDS  = %i[first_name last_name email gender locale language].freeze
+    METHODS = [*FIELDS, :name].flat_map { [_1, "#{_1}="] }.freeze
+
+    included do
+      delegate \
+        :to_s, :login_token,
+        *METHODS,
+        *User.genders.keys.map { "#{_1}?" },
+        to: :user
+
+      FIELDS.each do |attribute|
+        scope :"with_#{attribute}", ->(value) {
+          with_user User.where(attribute => value)
+        }
+      end
+
+      def self.method_missing(method, ...)
+        @method_missing_admin_users ||= {}
+        @method_missing_admin_users[method] ||=
+          with_first_name(method.capitalize).first or
+            super
+      end
+    end
+  end
+end
