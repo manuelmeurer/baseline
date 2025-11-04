8d113068566a5341de368208ab0d9f0c467d91e8
Manuel Meurer <manuel@meurer.io>
2025-11-04 10:42:54 +0100

    add autosubmit stimulus controller

---
+++ app/javascript/baseline/controllers/autosubmit_controller.js
@@ -0,0 +1,66 @@
+import ApplicationController from "application_controller"
+
+export default class extends ApplicationController {
+  static values = {
+    submitAfter: { type: Number, default: 500 }
+  }
+
+  connect() {
+    this.element.querySelectorAll("select, input[type='text'], input[type='checkbox'], input[type='radio']").forEach(input => {
+      input.addEventListener("change", () =>
+        this.waitForConstant("Turbo")
+          .then(() =>
+            this.element.requestSubmit()
+          )
+      )
+    })
+
+    this.element.querySelectorAll('input[type="text"], textarea')
+                .forEach(element => {
+
+      element.addEventListener("input", event => {
+        if (this.shouldIgnore(event))
+          return
+
+        // TODO: use this.debounce() here.
+        clearTimeout(this.timeout)
+        this.timeout = setTimeout(
+          () => this.submitForm(event.target),
+          this.submitAfterValue
+        )
+      })
+
+      element.addEventListener("focus", event => {
+        this.saveLastValue(event.target)
+      })
+
+      element.addEventListener("focusout", event => {
+        if (this.shouldIgnore(event))
+          return
+        clearTimeout(this.timeout)
+        this.submitForm(event.target)
+      })
+    })
+  }
+
+  shouldIgnore(event) {
+    return [
+      9,              // Tab
+      16, 17, 18,     // Shift, Alt, Ctrl
+      27,             // Caps lock
+      37, 38, 39, 40, // Arrows
+      91, 92, 93      // Windows keys
+    ].includes(event.which)
+  }
+
+  saveLastValue(element) {
+    element.dataset.lastValue = element.value
+  }
+
+  submitForm(element) {
+    if (element.value !== element.dataset.lastValue) {
+      this.saveLastValue(element)
+      element.closest("form").requestSubmit()
+    }
+  }
+}
