9d87a820a4f21f8c078afb42fe81a555be47d459
Manuel Meurer <manuel@meurer.io>
2025-11-04 12:04:16 +0100

    add toast and turbo response controller

---
+++ app/javascript/baseline/controllers/toast_controller.js
@@ -0,0 +1,21 @@
+import ApplicationController from "application_controller"
+import { Toast }             from "bootstrap"
+
+export default class extends ApplicationController {
+  static targets = ["container", "template"]
+
+  show(type, body) {
+    if (!["success", "error"].includes(type))
+      throw new Error(`${type} is not a valid toast type.`)
+
+    const element = this.templateTarget.cloneNode(true)
+
+    element.classList.add(type)
+    element.removeAttribute("data-toast-target")
+    element.querySelector(".toast-body").innerHTML = body.replace(/\n/g, "<br>")
+
+    this.containerTarget.prepend(element)
+
+    new Toast(element, { delay: 10000 }).show()
+  }
+}
---
+++ app/javascript/baseline/controllers/turbo_response_controller.js
@@ -0,0 +1,47 @@
+import ApplicationController from "application_controller"
+
+export default class extends ApplicationController {
+  static values = {
+    redirect:          String,
+    successMessage:    String,
+    errorMessage:      String,
+    closeModal:        Boolean,
+    reloadMain:        Boolean,
+    reloadMainOrModal: Boolean,
+    reloadFrames:      Array
+  }
+
+  connect() {
+    if (this.redirectValue)
+      if (document.modalController?.modalIsOpen)
+        document.modalController.url = this.redirectValue
+      else
+        Turbo.visit(this.redirectValue)
+
+    if (this.closeModalValue)
+      document.modalController?.closeModal()
+
+    if (this.reloadMainValue) {
+      this.main.reload()
+      this.navbar?.reload()
+    }
+
+    if (this.reloadMainOrModalValue)
+      if (document.modalController?.modalIsOpen)
+        document.modalController.reload()
+      else {
+        this.main.reload()
+        this.navbar?.reload()
+      }
+
+    this.reloadFramesValue?.forEach(id =>
+      document.querySelector(`turbo-frame#${id}`).reload()
+    )
+
+    if (this.successMessageValue)
+      this.showToast("success", this.successMessageValue)
+
+    if (this.errorMessageValue)
+      this.showToast("error", this.errorMessageValue)
+  }
+}
