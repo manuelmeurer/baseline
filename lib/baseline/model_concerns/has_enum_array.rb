9b970bd3d9ff603318d6fd27f2e0f4a894e97779
Manuel Meurer <manuel@meurer.io>
2025-11-11 18:47:02 +0100

    add has_enum_array

---
+++ lib/baseline/model_concerns/has_enum_array.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+module Baseline
+  module HasEnumArray
+    def self.[](attribute)
+      singular_attribute = attribute.to_s.singularize.to_sym
+      valid_method_name  = :"valid_#{attribute}"
+
+      Module.new do
+        extend ActiveSupport::Concern
+
+        included do
+          validates attribute, array_uniqueness: true
+
+          validate do
+            if invalids = public_send(attribute).difference(self.class.public_send(valid_method_name)).presence
+              errors.add attribute, message: "contain invalid elements: #{invalids.join(", ")}"
+            end
+          end
+
+          if default = try(:default_subscriptions)
+            attribute attribute, default: -> {
+              default.if(-> { _1 == :all }) {
+                public_send(valid_method_name)
+              }
+            }
+          end
+        end
+
+        define_method "#{attribute}=" do |value|
+          super Array(value).map(&:to_s)
+        end
+
+        define_method "add_#{singular_attribute}" do |value|
+          update! attribute => public_send(attribute).union([value.to_s])
+        end
+
+        define_method "remove_#{singular_attribute}" do |value|
+          update! attribute => public_send(attribute).excluding(value.to_s)
+        end
+
+        define_method "has_#{singular_attribute}?" do |value|
+          valid_values = self.class.public_send(valid_method_name)
+          if valid_values.exclude?(value.to_s)
+            raise "#{value} is not a valid value for #{attribute}, valid are: #{valid_values.join(", ")}}"
+          end
+
+          public_send(attribute).include?(value.to_s)
+        end
+      end
+    end
+  end
+end
