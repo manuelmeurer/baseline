4a3d42e59e2627e841e4b7a22ede3484cd156539
Manuel Meurer <manuel@meurer.io>
2025-11-16 15:17:03 +0100

    add acts as email confirmation

---
+++ lib/baseline/model_concerns/acts_as_email_confirmation.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+module Baseline
+  module ActsAsEmailConfirmation
+    extend ActiveSupport::Concern
+
+    included do
+      include HasEmail,
+              HasTimestamps[:confirmed_at, :expired_at, :revoked_at]
+
+      belongs_to :confirmable, polymorphic: true
+
+      validates :email, presence: true
+      validates :expired_at, presence: true
+      validates :revoked_at, absence: { if: :unconfirmed? }
+
+      validate on: :create, if: :confirmable do
+        if confirmable.email_confirmations.unconfirmed.expired_after.any?
+          errors.add :confirmable, message: "already has an unconfirmed and unexpired email confirmation"
+        end
+
+        if confirmable.email_confirmations.unconfirmed.exists?(email: confirmable.email) && email != confirmable.email
+          errors.add :confirmable, message: "has not confirmed their current email, so a new confirmation for another email cannot be created"
+        end
+      end
+    end
+
+    def active?
+      confirmed? && unrevoked?
+    end
+
+    def url
+      case confirmable
+      when User
+        namespace  = confirmable.userable.class.to_s.underscore.pluralize.to_sym
+        url_params = { t: confirmable.userable.login_token }
+      when FreelancerRequest
+        raise "implement"
+      else raise "Unexpected confirmable: #{confirmable.class}"
+      end
+
+      Rails.application.routes.url_helpers.url_for [
+        namespace,
+        :email_confirmation,
+        id: signed_id,
+        **url_params
+      ]
+    end
+  end
+end
