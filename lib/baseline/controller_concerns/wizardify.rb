7d48a880c5ee8fb5d9c147bca7ca83cdec67e9b2
Manuel Meurer <manuel@meurer.io>
2025-12-06 12:03:35 +0100

    rename partial, use helper method instead of partial param

--- lib/baseline/controller_concerns/wizardify.rb
+++ lib/baseline/controller_concerns/wizardify.rb
@@ -51,6 +51,7 @@ module Baseline
         helper_method def step_number   = steps.index(current_step) + 1
         helper_method def step_count    = steps.size
         helper_method def step_progress = (step_number.to_f * 100 / step_count).round
+        helper_method def frame_id      = :wizard
     end
 
     def index = redirect_to_first_or_next_form_step
