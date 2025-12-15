8ec579df05f7d86e9a62e9509d314b60153be3e7
Manuel Meurer <manuel@meurer.io>
2025-12-15 20:21:56 +0100

    refactor avo fields

--- lib/baseline/model_concerns/acts_as_avo_resource.rb
+++ lib/baseline/model_concerns/acts_as_avo_resource.rb
@@ -58,16 +58,32 @@ module Baseline
       end
     end
 
-    def email_field(attribute = :email)
-      field attribute, as: :text, only_on: :forms, default: params[attribute]
-      field attribute, as: :text, only_on: :display do
-        mail_to record.send(attribute)
-      end
+    def field(attribute, **options, &block)
+      return super if options.key?(:as)
+
+      field_options(attribute, options)
+        .unless(Array) { [_1] }
+        .each {
+          super attribute, **_1, &block
+        }
     end
 
-    def url_field(attribute = :url, **options)
-      field attribute,
-        **options.reverse_merge(
+    def field_options(attribute, options)
+      column                 = model_class.schema_columns[attribute]
+      attribute_suffix       = attribute.to_s.split("_").last.to_sym
+      association_reflection = model_class.reflections[attribute.to_s]
+      attachment_reflection  = model_class.reflect_on_all_attachments.detect { _1.name == attribute }
+
+      case
+      when attribute == :id
+        options.merge(as: :id)
+      when attribute_suffix == :email
+        [
+          options.merge(as: :text, only_on: :forms, default: params[attribute]),
+          options.merge(as: :text, only_on: :display, format_using: -> { mail_to value })
+        ]
+      when attribute_suffix == :url
+        options.reverse_merge(
           as:      :text,
           default: params[attribute],
           format_display_using: -> {
@@ -80,64 +96,70 @@ module Baseline
             end
           }
         )
-    end
-
-    def enum_field(attribute, **options)
-      field attribute,
-        **options.reverse_merge(
-          as:      :select,
-          default: params[attribute],
-          enum:    model_class.public_send(attribute.pluralize)
-        )
-    end
-
-    def locale_field(attribute = :locale, **options)
-      field attribute,
-        **options.reverse_merge(
+      when attribute_suffix == :locale
+        options.reverse_merge(
           as:      :select,
           default: params[attribute],
           options: Baseline::Avo::Filters::Language.new.options.invert
         )
-    end
-
-    def image_field(attribute = :photo)
-      field attribute,
-        as:       :file,
-        is_image: true,
-        only_on:  :forms
-      field attribute,
-        as:      :text,
-        only_on: :display,
-        format_using: -> {
-          if value.attached?
-            size = view == "index" ? :xs_thumb : :md_fit
-            link_to Rails.application.routes.url_helpers.url_for(value), **helpers.external_link_attributes do
-              render helpers.component(:attachment_image, value, size)
-            end
-          end
-        }
-    end
-
-    def belongs_to_field(attribute, **options)
-      field attribute,
-        **options.reverse_merge(
+      when attribute_suffix == :amount
+        options.reverse_merge(
+          as:      :text,
+          default: params[attribute],
+          format_display_using: -> {
+            value.format
+          }
+        )
+      when attachment_reflection.is_a?(ActiveStorage::Reflection::HasOneAttachedReflection)
+        [
+          options.reverse_merge(
+            as:       :file,
+            is_image: true,
+            only_on:  :forms
+          ),
+          options.reverse_merge(
+            as:      :text,
+            only_on: :display,
+            format_using: -> {
+              if value.attached?
+                size = view == "index" ? :xs_thumb : :md_fit
+                link_to Rails.application.routes.url_helpers.url_for(value), **helpers.external_link_attributes do
+                  render helpers.component(:attachment_image, value, size)
+                end
+              end
+            }
+          )
+        ]
+      when attachment_reflection.is_a?(ActiveStorage::Reflection::HasManyAttachedReflection)
+        options.merge(as: :has_many)
+      when model_class.defined_enums.key?(attribute.to_s)
+        choices = model_class
+          .public_send(attribute.pluralize)
+          .keys
+          .index_by {
+            model_class.human_enum_name attribute, _1
+          }
+        options.reverse_merge(
+          as:      :select,
+          default: params[attribute],
+          options: choices
+        )
+      when association_reflection.is_a?(ActiveRecord::Reflection::BelongsToReflection)
+        options.reverse_merge({
           as:         :belongs_to,
           default:    -> { params[:"#{attribute}_gid"]&.then { GlobalID.find(_1) } },
           searchable: true
-        )
-    end
-
-    def polymorphic_field(attribute, **options)
-      belongs_to_field attribute,
-        **options.reverse_merge(
-          polymorphic_as: attribute,
-          types:          polymorphic_types_with_resource(attribute)
-        )
-    end
-
-    def array_field(attribute, **options)
-      field attribute,
-        **options.reverse_merge(
+        }.if(association_reflection.options[:polymorphic]) {
+          _1.merge \
+            polymorphic_as: attribute,
+            types:          polymorphic_types_with_resource(attribute)
+        })
+      when association_reflection.class.in?([ActiveRecord::Reflection::HasManyReflection, ActiveRecord::Reflection::ThroughReflection])
+        options.merge(as: :has_many)
+      when association_reflection.is_a?(ActiveRecord::Reflection::HasOneReflection)
+        options.merge(as: :has_one)
+      when column && column[:array]
+        options.reverse_merge(
           as:      :textarea,
           default: params[attribute],
           format_using: -> {
@@ -149,6 +171,23 @@ module Baseline
             value.split("\n")
           }
         )
+      when column && column[:type] == :string
+        options.reverse_merge(as: :text, format_index_using: -> { value&.truncate(50) })
+      when attribute.end_with?("?") || (column && column[:type] == :boolean)
+        options.reverse_merge(as: :boolean)
+      when column && column[:type] == :text
+        options.reverse_merge(as: :textarea, format_index_using: -> { value&.truncate(50) })
+      when column && column[:type].in?(%i[json jsonb])
+        options.reverse_merge(as: :key_value)
+      when column && column[:type] == :datetime
+        options.reverse_merge(as: :date_time)
+      when column && column[:type] == :date
+        options.reverse_merge(as: :date)
+      when column && column[:type].in?(%i[float integer])
+        options.reverse_merge(as: :number)
+      else
+        raise "Unexpected attribute: #{attribute}"
+      end
     end
 
     def timestamp_fields
