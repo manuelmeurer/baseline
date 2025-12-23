c0fb33c4112ba7611378e64b5947ac838e4ba4d3
Manuel Meurer <manuel@meurer.io>
2025-12-23 22:54:54 +0100

    improve modal close button in wizard success step

--- lib/baseline/components/modal_close_button_component.rb
+++ lib/baseline/components/modal_close_button_component.rb
@@ -2,17 +2,20 @@
 
 module Baseline
   class ModalCloseButtonComponent < ApplicationComponent
-    def initialize(wrapper: true)
-      @wrapper = wrapper
+    def initialize(wrapper: true, text: nil, icon: :reject, button_color: "outline-dark")
+      @wrapper, @text, @icon, @button_color =
+        wrapper, text, icon, button_color
     end
 
     def call
       return unless ::Current.modal_request
 
-      link = link_to("#", class: "btn btn-outline-dark", data: { bs_dismiss: "modal" }) do
+      @text ||= t(:cancel).capitalize
+
+      link = link_to("#", class: "btn btn-#{@button_color}", data: { bs_dismiss: "modal" }) do
         safe_join [
-          component(:icon, :reject),
-          t(:cancel).capitalize
+          component(:icon, @icon),
+          @text
         ], " "
       end
 
