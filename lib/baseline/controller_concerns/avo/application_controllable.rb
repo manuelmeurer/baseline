9809a2320098019571931c71e3000c0c423e50fd
Manuel Meurer <manuel@meurer.io>
2025-12-29 22:28:33 +0100

    move to avo namespace

---
+++ lib/baseline/controller_concerns/avo/application_controllable.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Baseline
+  module Avo
+    module ApplicationControllable
+      extend ActiveSupport::Concern
+
+      included do
+        include Baseline::Authentication[:with_admin_user]
+
+        before_action prepend: true do
+          ::Current.namespace = :avo
+        end
+      end
+
+      def avo_login_url = Rails.application.routes.url_helpers.admin_login_url
+    end
+  end
+end
