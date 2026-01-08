4259311c94035d57b787a19f8818b60cc9b93ba6
Manuel Meurer <manuel@meurer.io>
2026-01-08 18:30:18 +0100

    fix plausible url

--- lib/baseline/helper.rb
+++ lib/baseline/helper.rb
@@ -342,7 +342,7 @@ module Baseline
         ::Current.namespace != :admin
 
       javascript_include_tag "/qwerty/js/script.js",
-        host:  "", # Use the current host instead of the asset host, if one is configured.
+        host:  request.base_url, # Use the request host instead of the asset host, if one is configured.
         defer: true,
         data: {
           domain: request.host,
