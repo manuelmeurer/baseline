830c7ec3669e7bf5a7ffa322c0d01e06749d44d2
Manuel Meurer <manuel@meurer.io>
2026-01-13 23:09:57 +0100

    formatting, use default button color

--- lib/baseline/components/wizard_actions_component.rb
+++ lib/baseline/components/wizard_actions_component.rb
@@ -2,10 +2,13 @@
 
 module Baseline
   class WizardActionsComponent < ApplicationComponent
-    def initialize(form:, step_label: nil, cancel_url: nil, show_current_step: true)
-      @form, @step_label, @cancel_url, @show_current_step =
-        form, step_label, cancel_url, show_current_step
-      @back_css_class = class_names("btn", "btn-outline-dark", "order-4", "order-sm-0", "btn-lg" => !::Current.modal_request)
+    def initialize(form:, step_label: nil, cancel_url: nil, show_current_step: true, button_color: "primary")
+      @form, @step_label, @cancel_url, @show_current_step, @button_color =
+        form, step_label, cancel_url, show_current_step, button_color
+      @back_css_class = class_names(
+        "btn", "btn-outline-dark", "order-4", "order-sm-0",
+        "btn-lg" => !::Current.modal_request
+      )
     end
 
     def before_render
@@ -13,7 +16,11 @@ module Baseline
     end
 
     private def submit_button(css_class: nil)
-      @form.button class: class_names("btn", "btn-primary", *css_class, "btn-lg" => !::Current.modal_request) do
+      css_class = class_names(
+        "btn", "btn-#{@button_color}", *css_class,
+        "btn-lg" => !::Current.modal_request
+      )
+      @form.button class: css_class do
         safe_join [
           t(helpers.last_step? ? :finish : :next).capitalize,
           component(:icon, :forward, style: :solid)
