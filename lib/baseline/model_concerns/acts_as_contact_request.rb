037fa15a8c60ed9f872b0897243fe814e6453590
Manuel Meurer <manuel@meurer.io>
2026-01-15 12:43:29 +0100

    add acts as contact request

---
+++ lib/baseline/model_concerns/acts_as_contact_request.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+module Baseline
+  module ActsAsContactRequest
+    def self.[](kinds)
+      Module.new do
+        extend ActiveSupport::Concern
+
+        included do
+          include HasEmail,
+                  HasFullName,
+                  HasLocale,
+                  HasTimestamps[:ignored_at]
+
+          enum :kind, kinds,
+            validate: true
+
+          has_many :messages,
+            class_name: "ContactRequestMessage",
+            as:         :recipient,
+            dependent:  :destroy
+
+          validates :name, presence: true
+          validates :email, presence: true
+          validates :message, presence: true
+        end
+
+        class_methods do
+          # This will generate corresponding scopes and methods.
+          def status_scopes
+            {
+              ignored:  nil,
+              messaged: %i[with_messages],
+              pending:  %i[unignored without_messages]
+            }
+          end
+        end
+
+        def to_s = "Contact request from #{name} (#{email})"
+
+        def fields = %i[name email phone company message]
+      end
+    end
+  end
+end
