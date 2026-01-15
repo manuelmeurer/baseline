7c568a51146d67bcf414c23ea287e6e6f24efae0
Manuel Meurer <manuel@meurer.io>
2026-01-15 14:51:24 +0100

    fix setting messageable

--- lib/baseline/avo/resources/message.rb
+++ lib/baseline/avo/resources/message.rb
@@ -7,11 +7,11 @@ module Baseline
         def fields
           field :id
           field :kind
-          field :recipient
+          field :recipient, readonly: true
           field :email_delivery, only_on: :show
           field :email_delivery_subject, as: :text, only_on: :new
           field :email_delivery_sections_md, as: :textarea, only_on: :new
-          field :messageable, only_on: :show
+          field :messageable, readonly: true
           timestamp_fields
           field :tasks
         end
