6dfe8508c021e7eff0c88e3718f61483a38b4e6f
Manuel Meurer <manuel@meurer.io>
2026-02-07 00:21:56 -0600

    enable logging with sentry

--- lib/baseline/initializers/sentry.rb
+++ lib/baseline/initializers/sentry.rb
@@ -5,8 +5,16 @@ require "sentry-rails"
 Sentry.init do |config|
   config.breadcrumbs_logger      = %i[active_support_logger http_logger]
   config.dsn                     = Rails.application.env_credentials.sentry&.dsn || "dummy"
-  config.excluded_exceptions    -= ["ActiveRecord::RecordNotFound"]
+  config.enable_logs             = true
+  config.enabled_patches << :logger
+  config.excluded_exceptions -= ["ActiveRecord::RecordNotFound"]
   config.include_local_variables = true
+  config.rails.structured_logging.subscribers = {
+    active_record:     Sentry::Rails::LogSubscribers::ActiveRecordSubscriber,
+    action_controller: Sentry::Rails::LogSubscribers::ActionControllerSubscriber,
+    active_job:        Sentry::Rails::LogSubscribers::ActiveJobSubscriber,
+    action_mailer:     Sentry::Rails::LogSubscribers::ActionMailerSubscriber
+  }
   config.release                 = Rails.configuration.revision
   config.send_default_pii        = true
 end
