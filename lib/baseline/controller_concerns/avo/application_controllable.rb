b3b9b3161f28d2ed7e0d84915b7898a365d57401
Manuel Meurer <manuel@meurer.io>
2026-02-08 03:09:58 -0600

    support search param in avo

--- lib/baseline/controller_concerns/avo/application_controllable.rb
+++ lib/baseline/controller_concerns/avo/application_controllable.rb
@@ -18,6 +18,30 @@ module Baseline
         before_action do
           params[:via_relation_class] ||= params[:via_belongs_to_resource_class]
         end
+
+        # Convert ?search=foo into encoded_filters for the Search filter.
+        before_action only: :index, if: -> { params[:search].present? } do
+          search_value = params.delete(:search)
+          resource_class = self
+            .class
+            .name
+            .delete_prefix("Avo::")
+            .delete_suffix("Controller")
+            .singularize
+            .then { "Avo::Resources::#{_1}" }
+            .constantize
+
+          unless resource_class.model_class.respond_to?(:search)
+            ReportError.call "Resource #{resource_class} was called with a search param, but it has no search filter configured."
+            redirect_to url_for(params.permit!)
+            next
+          end
+
+          encoded = ::Avo::Filters::BaseFilter.encode_filters(
+            Avo::Filters::Search.to_s => search_value
+          )
+          redirect_to url_for(params.permit!.merge(encoded_filters: encoded))
+        end
       end
 
       def avo_login_url = Rails.application.routes.url_helpers.admin_login_url
