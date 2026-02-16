1c41cdb6e77affba1be4e706736509b7ca521e86
Manuel Meurer <manuel@meurer.io>
2026-02-16 22:40:26 +0100

    add reason and details fields to avo deactivation action

--- lib/baseline/avo/actions/deactivate.rb
+++ lib/baseline/avo/actions/deactivate.rb
@@ -4,14 +4,27 @@ module Baseline
   module Avo
     module Actions
       class Deactivate < ::Avo::BaseAction
-        def handle(query:, **)
+        def fields
+          field :reason,
+            as: :select,
+            options: -> {
+              record
+                .deactivations
+                .build
+                .valid_reasons
+                .index_by(&:titleize)
+            }
+          field :details, as: :textarea
+        end
+
+        def handle(query:, fields:, **)
           process(
             query,
             condition:       -> { _1.active? },
             success_message: "deactivated successfully.",
             error_message:   "already deactivated."
           ) {
-            _1.deactivate!
+            _1.deactivate!(**fields)
           }
         end
       end
